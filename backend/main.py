62ae7e63dcfa47b865bb36c6d3c9acb86bc41dd5
Kevin Liu <kevinxo328@gmail.com>
2026-04-09 09:46:25 +0800

    feat: add cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,26 @@
 from ag_ui_langgraph import add_langgraph_fastapi_endpoint
 from copilotkit import LangGraphAGUIAgent
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.agents.test_agent.agent import graph
 
 app = FastAPI()
 
+CORS_ORIGINS = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 add_langgraph_fastapi_endpoint(
     app=app,
     agent=LangGraphAGUIAgent(
-        name='Test agent',
+        name='test_agent',
         description='An example agent to use as a starting point for your own agent.',
         graph=graph,
     ),
